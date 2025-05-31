5423fc6c50037a5bed583313fbf6712bc5bf8abc
Saif Islam Rayhan <134764307+saif-islam-rayhan@users.noreply.github.com>
2025-05-31 12:20:01 +0600

    Create ForeignPlayer.java

---
+++ BPL_Draft/ForeignPlayer.java
@@ -0,0 +1,8 @@
+package bplnilam;
+
+public class ForeignPlayer extends Player {
+    public ForeignPlayer(String iccid, String name, String nationality,
+                         int jerseyNumber, String role) {
+        super(iccid, name, nationality, jerseyNumber, role);
+    }
+}
