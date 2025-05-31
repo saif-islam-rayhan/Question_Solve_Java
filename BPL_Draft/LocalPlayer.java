0a2638a26da62e589203cb05c77989b95bb51e90
Saif Islam Rayhan <134764307+saif-islam-rayhan@users.noreply.github.com>
2025-05-31 12:19:17 +0600

    Create LocalPlayer.java

---
+++ BPL_Draft/LocalPlayer.java
@@ -0,0 +1,8 @@
+package bplnilam;
+
+public class LocalPlayer extends Player {
+    public LocalPlayer(String iccid, String name, String nationality,
+                       int jerseyNumber, String role) {
+        super(iccid, name, nationality, jerseyNumber, role);
+    }
+}
